ab6324bfa46e7f90cea620b6e2ca481dd31b7a70
Tony <97451137+TonybynMp4@users.noreply.github.com>
2023-10-04 00:51:14 +0200

    ça avance

--- js/main.js
+++ js/main.js
@@ -1,159 +1,109 @@
 let Listes = []
 document.addEventListener('DOMContentLoaded', () => {
-  document.getElementById('addList').addEventListener("click", () => {
-    const name = prompt("nom")
-    const id = Listes.length
-    addList(id, name, [])
-  })
+    document.getElementById('addList').addEventListener("click", () => {
+        const name = prompt("nom")
+        const id = Listes.length
+        createList(id, name, [])
+    })
 });
 
 class Liste {
-  constructor(id, name, tasks){
-    this.id = id
-    this.name = name;
-    this.tasks = tasks
-  }
+    constructor(id, name, tasks) {
+        this.id = id
+        this.name = name;
+        this.tasks = tasks
+    }
 }
 
 class Task {
-  constructor(id, name, date) {
-    this.id = id,
-    this.name = name;
-    this.date = date;
-    this.done = false;
-  }
+    constructor(id, name, date) {
+        this.id = id,
+            this.name = name;
+        this.date = date;
+        this.done = false;
+    }
 
-  check() {
-    this.done = !this.done;
-  }
+    check() {
+        this.done = !this.done;
+    }
 
-  edit() {
-    
-  }
+    edit(newName, newDate) {
+        this.name = newName || this.name;
+        this.date = newDate || this.date;
+    }
 }
 
-const toDoApp = {
-  listes: [],
-    
-  creerListe(nom, date) {
-    const nouvelleListe = new Liste(nom, date);
-    this.listes.push(nouvelleListe);
-    return nouvelleListe;
-  },
-
-
-  editerListe(nomListe, nouveauNom) {
-    const liste = this.listes.find(liste => liste.nom === nomListe);
-    if (liste) {
-      liste.nom = nouveauNom;
-    }
-  },
-
-  editerTache(nomListe, nomTache, nouvelleNom, nouvelleDate, nouvelleLimite) {
-    const liste = this.listes.find(liste => liste.nom === nomListe);
-    if (liste) {
-      const tache = liste.taches.find(tache => tache.nom === nomTache);
-      if (tache) {
-        tache.nom = nouvelleNom;
-        tache.date = nouvelleDate;
-        tache.limite = nouvelleLimite;
-      }
-    }
-  },
-
-  cocherTache(nomListe, nomTache, etat) {
-    const liste = this.listes.find(liste => liste.nom === nomListe);
-    if (liste) {
-      const tache = liste.taches.find(tache => tache.nom === nomTache);
-      if (tache) {
-        tache.faite = etat;
-      }
-    }
-  }
-};
-
-function editerTache() {
-  const listeSelectionnee = listeListes.querySelector('.selectionnee');
-  const tacheSelectionnee = listeTaches.querySelector('.selectionnee');
-  if (listeSelectionnee && tacheSelectionnee) {
-    const nomListe = listeSelectionnee.textContent;
-    const nomTache = tacheSelectionnee.textContent.split(',')[0].slice(7);
-    const nouveauNom = prompt('Entrez le nouveau nom de la tâche :');
-    const nouvelleDate = prompt('Entrez la nouvelle date de la tâche :');
-    const nouvelleLimite = prompt('Entrez la nouvelle limite de la tâche :');
-    if (nouveauNom !== null && nouvelleDate !== null && nouvelleLimite !== null) {
-      toDoApp.editerTache(nomListe, nomTache, nouveauNom, nouvelleDate, nouvelleLimite);
-      tacheSelectionnee.textContent = `Tâche: ${nouveauNom}, Date: ${nouvelleDate}`;
-    }
-  }
+function createListElement(id, name) {
+    let Element = document.createElement("div");
+    Element.classList.add("list");
+    Element.setAttribute("id", "liste" + id);
+    Element.innerHTML = `
+        <div class="list-header">
+            <h2>${name}</h2>
+            <div class="list-header-buttons">
+                <button class="button">Ajouter Tache</button>
+                <button class="button">Supprimer Liste</button>
+            </div>
+        </div>
+        <div class="list-tasks"></div>
+    `;
+    return Element
 }
 
-function cocherTache() {
-  const tacheSelectionnee = listeTaches.querySelector('.selectionnee');
-  if (tacheSelectionnee) {
-    const nomListe = listeListes.querySelector('.selectionnee').textContent;
-    const nomTache = tacheSelectionnee.textContent.split(',')[0].slice(7);
-    const etat = confirm('La tâche est-elle faite ?');
-    toDoApp.cocherTache(nomListe, nomTache, etat);
-    tacheSelectionnee.style.textDecoration = etat ? 'line-through' : 'none';
-  }
+function addListButtonEvents(id) {
+    console.log(document.getElementById("liste" + id))
+    document.getElementById("liste" + id).getElementsByClassName("button")[0].addEventListener("click", () => {
+        const taskName = prompt("task name")
+        const taskDate = prompt("date limite")
+        const newTaskId = Listes[id].tasks.length
+        if (taskName === null) {
+            return
+        }
+        createTask(id, taskName, taskDate, newTaskId)
+    })
+    document.getElementById("liste" + id).getElementsByClassName("button")[1].addEventListener("click", () => {
+        document.getElementById("liste" + id).remove()
+        Listes.splice(id, 1)
+    })
 }
 
-function addTask (liste, nom, date, limite) {
-  const nouvelleTache = new Tache(nom, date, limite);
-  liste.taches.push(nouvelleTache);
-  return nouvelleTache;
-
+function createList(id, name, tasks) {
+    let list = new Liste(id, name, tasks);
+    Listes[id] = list;
+    document.getElementById("lists-container").appendChild(createListElement(id, name));
+    addListButtonEvents(id)
 }
 
-function addList (id, name, tasks){
-  let list = new Liste(id, name, tasks);
-  Listes[id] = list;
-  let Element = document.createElement("div");
-  Element.classList.add("list");
-  Element.setAttribute("id","liste" + id);
-  Element.innerHTML = `
-    <div class="list-header">
-      <h2>${list.name}</h2>
-      <button class="list-button">Ajouter Tache</button>
-      <button class="list-button">Supprimer Liste</button>
-      </div>
-    <div class="list-tasks"></div>
+function createTaskElement(id, name, date) {
+    let Element = document.createElement('div');
+    Element.classList.add('task');
+    Element.setAttribute("id", "task" + id);
+    Element.innerHTML = `
+    <input type="checkbox" class="isDone" name="done"/>
+    <input type="text" class="task-name" value="${name}"/>
+    <span>${date}</span>
+    <button class="button">🗑️</button>
   `;
-  document.getElementById("lists-container").appendChild(Element);
-  document.getElementById("liste"+id).getElementsByClassName("list-button")[0].addEventListener("click", () => {
-    const taskName=prompt("task name")
-    const taskDate = prompt("date limite")
-    const newTaskId = Listes[id].tasks.length
-    if (taskName ===  null) {
-      return
-    }
-    createTask(id, taskName, taskDate, newTaskId)
-  })
+    return Element
 }
 
-function createTask (listeId, name , date, id){
-  let task = new Task (id, name, date);
-  Listes[listeId].tasks[id] = task;
-  let Element = document.createElement('div');
-  Element.classList.add('task');
-  Element.setAttribute ("id", "task"+id);
-  Element.innerHTML = `
-    <input type="checkbox" class="done" name="done"/>
-    <label for="done">${name}</label>
-  `; 
-  let liste = document.getElementById("liste" + listeId);
-  console.log(liste, id)
-  liste.getElementsByClassName("list-tasks")[id].appendChild(Element);
+function addTaskButtonEvents(liste, id) {
+    const listeId = liste.id.replace("liste", "")
+    const task = liste.getElementsByClassName("list-tasks")[0].getElementsByClassName("task")[id]
+    task.getElementsByClassName("isDone")[0].addEventListener("change", (event) => {
+        if (event.currentTarget.checked) {
+            Listes[listeId].tasks[id].done = true
+        } else {
+            Listes[listeId].tasks[id].done = false
+        }
+    })
+}
 
-  liste.getElementsByClassName("list-tasks")[id].getElementsByClassName("done")[0].addEventListener("change", (event) => {
-    if (event.currentTarget.checked) {
-      Listes[listeId].tasks[id].done = true
-    } else {
-      Listes[listeId].tasks[id].done = false
-    }
-    console.log(Listes[listeId].tasks[id])
-  })
+function createTask(listeId, name, date, id) {
+    let task = new Task(id, name, date);
+    Listes[listeId].tasks[id] = task;
 
-
+    let liste = document.getElementById("liste" + listeId);
+    liste.getElementsByClassName("list-tasks")[0].appendChild(createTaskElement(id, name, date));
+    addTaskButtonEvents(liste, id)
 }
