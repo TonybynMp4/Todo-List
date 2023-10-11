004b26c9df0ee499be6161ab995e5c7f0aac3f1b
Tony <97451137+TonybynMp4@users.noreply.github.com>
2023-10-11 21:12:42 +0200

    Edit list Title & filters

--- js/list.js
+++ js/list.js
@@ -21,14 +21,17 @@ class List {
 
 function createListElement(id, name) {
     let Element = document.createElement("div");
+    const listId = "list" + id
     Element.classList.add("list");
-    Element.setAttribute("id", "list" + id);
+    Element.setAttribute("id", listId);
     Element.innerHTML = `
         <div class="list-header">
-            <h2>${name}</h2>
+            <h2 class="list-header-title">${name}</h2>
             <div class="list-header-buttons">
-                <button type="button">Ajouter Tache</button>
-                <button type="button">Supprimer Liste</button>
+                <button type="button" class="filter-button">Filtrer</button>
+                <button type="button" class="addTask-button">Ajouter Tache</button>
+                <button type="button" class="edit-button">Modifier Titre</button>
+                <button type="button" class="delete-button">Supprimer Liste</button>
             </div>
         </div>
         <div class="list-tasks"></div>
@@ -36,11 +39,56 @@ function createListElement(id, name) {
     return Element
 }
 
+function filterTasks(tasks, filterState) {
+    for (let i = 0; i < tasks.length; i++) {
+        const task = tasks[i]
+        const checkedTask = task.getElementsByTagName("input")[0].checked
+
+        if (filterState === 0) {
+            task.style.display = checkedTask && "none" || "flex"
+        } else if (filterState === 1) {
+            task.style.display = !checkedTask && "none" || "flex"
+        } else {
+            task.style.display = "flex"
+        }
+    }
+}
+
 function addListButtonEvents(listId) {
-    document.getElementById("list" + listId).getElementsByTagName("button")[0].addEventListener("click", () => {
+    let filterState = 0
+    const listElement = document.getElementById("list" + listId)
+    const titleElement = listElement.getElementsByTagName("h2")[0]
+
+    listElement.getElementsByClassName("addTask-button")[0].addEventListener("click", (e) => {
+        e.preventDefault()
         openModal(true, listId)
     })
-    document.getElementById("list" + listId).getElementsByTagName("button")[1].addEventListener("click", () => {
+    listElement.getElementsByClassName("filter-button")[0].addEventListener("click", (e) => {
+        e.preventDefault()
+        const tasks = listElement.getElementsByClassName("list-tasks")[0].getElementsByClassName("task")
+        filterTasks(tasks, filterState)
+        filterState = (filterState + 1) % 3
+    })
+    listElement.getElementsByClassName("edit-button")[0].addEventListener("click", (e) => {
+        e.preventDefault()
+        titleElement.setAttribute("contenteditable", "true")
+        titleElement.focus()
+    })
+
+    // Si focusout ou keydown Enter alors on enlève l'attribut contenteditable et on enregistre les changements
+    titleElement.addEventListener("focusout", (event) => {
+        event.preventDefault()
+        titleElement.setAttribute("contenteditable", "false")
+        Lists[listId].edit(titleElement.innerText)
+    })
+    titleElement.addEventListener("keydown", (event) => {
+        if (event.key === "Enter") {
+            event.preventDefault()
+            titleElement.setAttribute("contenteditable", "false")
+        }
+    })
+
+    listElement.getElementsByClassName("delete-button")[0].addEventListener("click", () => {
         document.getElementById("list" + listId).remove()
         Lists.splice(listId, 1)
     })
