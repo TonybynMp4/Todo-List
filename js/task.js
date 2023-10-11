fd3df2fb536bc02006097586dc9062710a467ee7
Tony <97451137+TonybynMp4@users.noreply.github.com>
2023-10-11 21:12:18 +0200

    Edit task update

--- js/task.js
+++ js/task.js
@@ -21,31 +21,52 @@ function createTaskElement(listId, task) {
     Element.classList.add('task');
     Element.setAttribute("id", "list"+listId+".task" + task.id);
     Element.innerHTML = `
-    <input type="checkbox" name="done"/>
-    <input type="text" placeholder="Tâche" class="task-name" name="task-name" value="${task.name}"/>
-    <input type="date" placeholder="00-00-0000" class="task-date" value="${task.date}"/>
-    <button type="button" class="delete-button">🗑️</button>
-  `;
+        <input type="checkbox" name="done"/>
+        <p class="task-name">${task.name}</p>
+        <input type="date" placeholder="00-00-0000" class="task-date" value="${task.date}"/>
+        <div class="task-buttons">
+            <button type="button" class="edit-button">✏️</button>
+            <button type="button" class="delete-button">🗑️</button>
+        </div>
+    `;
     return Element
 }
 
-function addTaskButtonEvents(listElement, id) {
+function addTaskButtonEvents(listElement, taskId) {
     const listId = listElement.id.replace("list", "")
     const list = Lists[listId]
     const tasksElement = listElement.getElementsByClassName("list-tasks")[0].getElementsByClassName("task")
     const taskElement = tasksElement[tasksElement.length-1]
+    const taskNameElement = taskElement.getElementsByTagName("p")[0]
+
     taskElement.getElementsByTagName("input")[0].addEventListener("change", () => {
-        list.tasks[id].check()
+        list.tasks[taskId].check()
     })
-    taskElement.getElementsByTagName("input")[1].addEventListener("change", (event) => {
-        list.tasks[id].edit(event.currentTarget.value)
+    taskElement.getElementsByTagName("input")[1].addEventListener("change", (e) => {
+        list.tasks[taskId].edit(null, e.currentTarget.value)
     })
-    taskElement.getElementsByTagName("input")[2].addEventListener("change", (event) => {
-        list.tasks[id].edit(null, event.currentTarget.value)
+    taskElement.getElementsByClassName("edit-button")[0].addEventListener("click", (e) => {
+        e.preventDefault()
+        taskNameElement.setAttribute("contenteditable", "true")
+        taskNameElement.focus()
     })
+
+    // Si focusout ou keydown Enter alors on enlève l'attribut contenteditable et on enregistre les changements
+    taskNameElement.addEventListener("focusout", (e) => {
+        e.preventDefault()
+        taskNameElement.setAttribute("contenteditable", "false")
+        Lists[listId].tasks[taskId].edit(taskNameElement.innerText, null)
+    })
+    taskNameElement.addEventListener("keydown", (e) => {
+        if (e.key === "Enter") {
+            e.preventDefault()
+            taskNameElement.setAttribute("contenteditable", "false")
+        }
+    })
+
     taskElement.getElementsByClassName("delete-button")[0].addEventListener("click", () => {
         taskElement.remove()
-        list.removeTask(id)
+        list.removeTask(taskId)
     })
 }
 
